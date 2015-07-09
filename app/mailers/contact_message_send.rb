9e11c12f37d1da0e2cfc89d139bf3035a4cebd7c
SaravananNV <saravanannagarajannv@gmail.com>
2015-07-09 17:44:02 +0530

    successfully send messages to spree.assist gmail

--- app/mailers/contact_message_send.rb
+++ app/mailers/contact_message_send.rb
@@ -1,11 +1,13 @@
 class ContactMessageSend < ApplicationMailer
-   # default to: ENV["GMAIL_USERNAME"]
+    default from: ENV["GMAIL_USERNAME"]
+
   def sample_email(name,message,client_email)
     @user = name
-    @subject =  "Enquiry Message - Received an Enquiry Message from #{name}"
+    @subject =  "Enquiry Message - Received an Enquiry Message from #{name} - Email - #{client_email}"
     @message = message
+    @recipent = "spree.assist@gmail.com"
     @metron_admin = ENV["GMAIL_USERNAME"]
     @client_email = client_email
-    mail(from: client_email, to: @metron_admin, subject: @subject)
+    mail(:to => @recipent ,:subject => @subject)
   end
 end
